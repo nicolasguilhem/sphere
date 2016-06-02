82d0d77fb18f98c0fa2ee69bf6148411bbf7fc29
nicolasguilhem <nicolas.guilhem@free.fr>
2016-06-02 23:21:26 +0200

    Innit

---
+++ typings/browser.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="browser\ambient\angular-animate\angular-animate.d.ts" />
+/// <reference path="browser\ambient\angular-material\angular-material.d.ts" />
+/// <reference path="browser\ambient\angular-route\angular-route.d.ts" />
+/// <reference path="browser\ambient\angular-ui-router\angular-ui-router.d.ts" />
+/// <reference path="browser\ambient\angular\angular.d.ts" />
+/// <reference path="browser\ambient\jquery\jquery.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="main\ambient\angular-animate\angular-animate.d.ts" />
+/// <reference path="main\ambient\angular-material\angular-material.d.ts" />
+/// <reference path="main\ambient\angular-route\angular-route.d.ts" />
+/// <reference path="main\ambient\angular-ui-router\angular-ui-router.d.ts" />
+/// <reference path="main\ambient\angular\angular.d.ts" />
+/// <reference path="main\ambient\jquery\jquery.d.ts" />
